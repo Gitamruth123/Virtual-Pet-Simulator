e75d5b645783de0729ed08f000ea7001d1efa17b
Amrutha Venugopal <Avenugopal4776@conestogac.on.ca>
2024-05-29 19:05:57 -0400

    Add project files.

---
+++ Virtual Pet Simulator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VirtualPet
+{
+    class Pet
+    {
+        static void Main(string[] args)
+        { // Variable declaration
+            string petType = " ";
+            string petName = " ";
+            int hunger = 6;
+            int happiness = 7;
+            int health = 8;
+
+            // Pet Creation
+            Console.WriteLine("Please choose a pet type: ");
+            Console.WriteLine("Cat");
+            Console.WriteLine("Dog");
+            Console.WriteLine("Rabbit");
+            petType = Console.ReadLine();
+
+            Console.WriteLine("You have choosen a" + " " + petType + "." + " " + "what would you like to name your pet?");
+            petName = Console.ReadLine();
+
+            Console.WriteLine("/nWelcome," + " " + petName + "." + "Lets take good care of him");
+
+        }
+    }
+}
