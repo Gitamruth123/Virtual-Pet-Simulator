dbfeda392dbcc46133e6e91ef1bb3882e87882bd
Amrutha Venugopal <Avenugopal4776@conestogac.on.ca>
2024-05-29 20:50:57 -0400

    1.Pet status is displaying. 2. Feed Action.

--- Virtual Pet Simulator/Program.cs
+++ Virtual Pet Simulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace VirtualPet
 {
@@ -22,8 +23,36 @@ namespace VirtualPet
             Console.WriteLine("You have choosen a" + " " + petType + "." + " " + "what would you like to name your pet?");
             petName = Console.ReadLine();
 
-            Console.WriteLine("/nWelcome," + " " + petName + "." + "Lets take good care of him");
+            Console.WriteLine("\nWelcome," + " " + petName + "." + "Lets take good care of him");
 
+            // Game start here
+            while (true)
+            {
+                // To display Pet status
+                Console.WriteLine("\nPet's status -Hunger:" + hunger + " " + "Happiness:" + happiness + " " + "Health:" + health);
+
+                // Pet Care Actions
+                Console.WriteLine("\nChoose an action: 1.Feed 2.Play 3.Rest 4.Exit");
+                string action = Console.ReadLine();
+
+                if (action == "1")
+                {
+                    //Feed
+                    hunger -= 2;
+                    if(hunger < 0)
+                    {
+                        hunger = 0;
+                    }
+                    health += 1;
+                    if(health > 10)
+                    {
+                        health = 10;
+   
+                    }
+
+                    Console.WriteLine(petName + " has been fed.");
+                }
+            }
         }
     }
 }
